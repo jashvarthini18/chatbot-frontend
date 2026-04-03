15d6402120db779b751b2ed4a81e8384c720657b
Jashvarthini Rajesh <jashvarthinirajesh@gmail.com>
2026-04-03 20:47:54 +0530

    Fix: CORS

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,5 @@
-// const BASE_URL = import.meta.env.VITE_API_URL;
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_API_URL;
+// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export async function signup(data) {
   const res = await fetch(`${BASE_URL}/auth/signup`, {
--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
-// const BASE_URL = import.meta.env.VITE_API_URL;
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_API_URL;
+// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export async function apiFetch(url, options = {}) {
   const token = localStorage.getItem("token");
